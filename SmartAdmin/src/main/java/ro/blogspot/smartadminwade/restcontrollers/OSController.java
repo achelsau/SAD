9f761bab32ed14759d9a079d58ab95b8b96f56f1
achelsau <ariel.chelsau@gmail.com>
2014-12-01 22:31:03 +0200

    #2 added @ResponseBody to the controller

--- SmartAdmin/src/main/java/ro/blogspot/smartadminwade/restcontrollers/OSController.java
+++ SmartAdmin/src/main/java/ro/blogspot/smartadminwade/restcontrollers/OSController.java
@@ -1,12 +1,10 @@
 package ro.blogspot.smartadminwade.restcontrollers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ro.blogspot.smartadminwade.model.OS;
 
@@ -19,6 +17,7 @@ import ro.blogspot.smartadminwade.model.OS;
 public class OSController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
 	public OS getListOfAvailableOSes(ModelMap model) {
 		
 		return new OS("Windows", "7", "x64");
